8979a66013a2127dac235ab09907f9795466b267
MilanSztranyai <m.sztranyai@gmail.com>
2024-06-04 19:35:47 +0200

    Code sorted, data sheet added

---
+++ Python code/Params.py
@@ -0,0 +1,10 @@
+subfolder = "Copper_Big"
+matName = "CopperBig"
+
+SampleRate = 125000000
+realOhmRes = 1050 #Ellenallas + tekercs ohmos ellenallasa
+#Kondenzatort merheted is mint ohmos ellenallast vagy illesztheted is
+
+clipData = True
+clipAtFreqMax = 2000
+clipAtFreqMin = 0
