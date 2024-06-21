7d42b1dc1f05f1a9349ff6509e2811d2697df0b4
BA77 <119893995+Gallius8977@users.noreply.github.com>
2024-06-21 10:56:56 +0200

    Phase (tbf)

--- Command.py
+++ Command.py
@@ -1,10 +1,13 @@
 import FileLister
 import DataProcessor
 import Params
+import Phase
 
 FileLister.main_activity()
 
 DataProcessor.main_activity()
+
+Phase.main_activity()
 
 '''if Params.clipData:
     DataClipper.main_activity()
