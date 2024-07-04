f1c6555573b3d19ab7c60b1b34e16de2365e6e1b
BA77 <119893995+Gallius8977@users.noreply.github.com>
2024-07-04 14:39:46 +0200

    Data

--- Params.py
+++ Params.py
@@ -1,6 +1,6 @@
-subfolder = "1Cu_Solid"
+subfolder = "R20_Al"
 matName = "Fe"
-ResultsPath = "Measurements/ProcessedData/4Al_Solid.txt"
+ResultsPath = "Measurements/ProcessedData/R20_Al.txt"
 
 FullTime = 76 #Empty: 152 us [76]
 SampleRate = 125000000 #Empty: 50.000.000 samp/sec [125]
