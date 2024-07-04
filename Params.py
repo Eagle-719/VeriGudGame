7629a93b783bc75c9002fa89fa7ab9caa6e244ee
BA77 <119893995+Gallius8977@users.noreply.github.com>
2024-07-04 19:51:17 +0200

    R20_Empty

--- Params.py
+++ Params.py
@@ -1,6 +1,6 @@
-subfolder = "R21_Empty"
+subfolder = "R20_Empty"
 matName = "Fe"
-ResultsPath = ("Results/ProcessedData/R21_Empty.txt")
+ResultsPath = ("Results/ProcessedData/R20_Empty.txt")
 
 FullTime = 76 #Empty: 152 us [76]
 SampleRate = 125000000 #Empty: 50.000.000 samp/sec [125]
