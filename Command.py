387aa0f2195c37f8d9eb36beb76af0e901f00e24
BA77 <119893995+Gallius8977@users.noreply.github.com>
2024-07-04 19:42:22 +0200

    R21_Empty sinfit

--- Command.py
+++ Command.py
@@ -1,12 +1,11 @@
 import FileLister
 import DataProcessorN
-import AMP
 
 FileLister.main_activity()
 
 DataProcessorN.main_activity()
 
-AMP.main_activity()
+#AMP.main_activity()
 
 '''if Params.clipData:
     DataClipper.main_activity()
