ce4050168947e8ce14b7618ecbdc70d5d7afcf2e
BA77 <119893995+Gallius8977@users.noreply.github.com>
2024-07-04 15:17:29 +0200

    random

--- Command.py
+++ Command.py
@@ -1,11 +1,12 @@
 import FileLister
 import DataProcessorN
+import AMP
 
 FileLister.main_activity()
 
 DataProcessorN.main_activity()
 
-#Phase.main_activity()
+AMP.main_activity()
 
 '''if Params.clipData:
     DataClipper.main_activity()
--- Params.py
+++ Params.py
@@ -1,11 +1,11 @@
-subfolder = "R21_Cu"
+subfolder = "10Empty"
 matName = "Fe"
-ResultsPath = "Results/ProcessedData/R21_Cu.txt"
+ResultsPath = "Results/ProcessedData/10Empty.txt"
 
 FullTime = 76 #Empty: 152 us [76]
 SampleRate = 125000000 #Empty: 50.000.000 samp/sec [125]
-realOhmRes = 500
-realOhmInd = 16.4 #Capacitor?
+realOhmRes = 289
+realOhmInd = 700 #Capacitor?
 
 clipData = False
 clipAtFreqMax = 2000
