07e6aba5b433e127c77a99a7430f68a812863669
BA77 <119893995+Gallius8977@users.noreply.github.com>
2024-07-05 20:45:01 +0200

    fitting img, res values and organization

--- Params.py
+++ Params.py
@@ -1,9 +1,9 @@
-subfolder = "10Empty"
-ResultsPath = ("Results/ProcessedData/10Empty.txt")
+subfolder = "C2_Empty"
+ResultsPath = ("Results/ProcessedData/C2_Empty.txt")
 
 FullTime = 76 #Empty: 152 us [76]
 SampleRate = 125000000 #Empty: 50.000.000 samp/sec [125]
-realOhmRes = 289
+realOhmRes = 1041
 Capacitor = 0.000000000272
 
 clipData = False
